afa9b989cde07c981d3d96b73865eeec082a45bd
Abhishek Patinge <abhishekpatinge2000@gamil.com>
2024-08-06 11:17:05 +0530

    Updated uploadFiles route to handle circular references

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ app.get('/uploadFiles', (req, res) => {
     try {
         console.log('heders---'+req.headers); // Log the headers to ensure the File-ID is received
         console.log('body'+req.body); // Log the body (if any)
-        res.send('Files upload endpoint hit'); // Send a response back to the client
+        res.send(req.headers); // Send a response back to the client
+
     } catch (error) {
         console.error('Error processing request:', error);
         res.status(500).send('Internal Server Error'); // Send a proper error response
